0cd09491474d68bfe81eec6378e6616fee1cf5fa
Edwaenaa <123628304+Edwaenaa@users.noreply.github.com>
2023-01-26 12:17:18 +0300

    Create test.js

---
+++ komutlar/test.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name:"test",
+    code:`
+   $SendMessage[
+   {newEmbed:}
+   {title:Bot çalışıyor.}
+   {description: **[Elit Code](https://discord.gg/3eak4Dq7AY)**}
+   {color:RANDOM}
+   `
+   }}
