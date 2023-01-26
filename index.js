fb8b1c53ec8f03899117985ebd15a3f276f6e763
Edwaenaa <123628304+Edwaenaa@users.noreply.github.com>
2023-01-26 12:19:08 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const aoijs = require("aoi.js");
+const ayarlar = require("./ayarlar.json")
+ const bot = new aoijs.Bot({
+   token: ayarlar.token,
+   prefix: ayarlar.prefix,
+   intents: "all"
+ });
+const loader = new aoijs.LoadCommands(bot)
+ loader.load(bot.cmd,"./komutlar/")
+  
+bot.onMessage() 
+bot.command({
+  name: "ping",
+  code: `Pingim \`$ping ms\` ` 
+})
+
+bot.status({
+  text: "By E!it Code",
+  type: "PLAYING",
+})
+
+bot.variables({
+Elit Code: "<3",
+aoijs:"<3"
+})
